7023a71ccf669795e54c4eff33fa047bc34651ba
sly750 <calvarez1007@yahoo.com>
2024-07-14 11:24:05 -0400

    Tons of modifying/learning. Added index.Js

---
+++ index.js
@@ -0,0 +1,72 @@
+/*
+function greet (){
+    console.log('hello')
+}
+greet()
+
+let name = 'christian';
+
+function greetUser (){
+    alert(`hello ${name}`)
+} 
+greetUser()
+
+
+function welcome (name, greet){
+      `I said ${greetUser}`
+}
+welcome(name, greet) 
+*/
+
+/*let isRunning = true;
+//let isRunning = false;
+
+    
+    function greet () {
+    let username = window.prompt(`Enter your username`);
+    
+    if (username == "")
+        console.log(alert('That is not a username'))
+        else 
+            console.log(alert(`Hello ${username}`))        
+    
+    }
+        console.log('program not running')
+
+isRunning ? greet() : false */
+
+/*
+function task1 (taskTwo){
+    console.log('Completed task 1');
+    taskTwo = task2()
+
+}
+function task2 (taskThree){
+    console.log('Completed task 2')
+    taskThree = task3()
+
+}
+function task3 (){
+    console.log('Completed all tasks')
+
+}
+task1() */ 
+
+let fruits = ['apple', 'banana', 'mango']
+let milk = [fruits, 'oat milk', 'cow milk', 'no milk']
+
+//fruits.push('coconut')
+
+/* 
+console.log(fruits[0])
+console.log(fruits[1])
+console.log(fruits[2])
+
+console.log (milk)
+*/
+
+
+
+
+
+
