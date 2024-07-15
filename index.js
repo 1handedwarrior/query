2b47a2a4d8a71c5a7814e4a90aae353150f7bf00
sly750 <calvarez1007@yahoo.com>
2024-07-15 11:35:42 -0400

    Adding more code to indexJs

--- index.js
+++ index.js
@@ -2,25 +2,24 @@
 function greet (){
     console.log('hello')
 }
-greet()
+//greet()
 
-let name = 'christian';
+let name = 'chris';
 
 function greetUser (){
+    let name = 'chris'
     alert(`hello ${name}`)
 } 
 greetUser()
-
+/*
 
 function welcome (name, greet){
       `I said ${greetUser}`
 }
 welcome(name, greet) 
-*/
 
-/*let isRunning = true;
-//let isRunning = false;
 
+/*let isRunning = true
     
     function greet () {
     let username = window.prompt(`Enter your username`);
@@ -51,21 +50,70 @@ function task3 (){
 
 }
 task1() */ 
-
+/*
 let fruits = ['apple', 'banana', 'mango']
-let milk = [fruits, 'oat milk', 'cow milk', 'no milk']
+let milk = ['oat milk', 'no milk', 'cow milk']
 
-//fruits.push('coconut')
+fruits.push('coconut')
+fruits.push('watermelon')
 
-/* 
 console.log(fruits[0])
 console.log(fruits[1])
 console.log(fruits[2])
+console.log(fruits[3])
+console.log(fruits[4])
+let order = console.log(`I want ${fruits[2]} with ${milk[0]} please`)
 
-console.log (milk)
+    function myOrder (){
+        alert('Hi welcome to DUNKIN');
+        () =>
+            prompt('What would you like to drink?')
+        
+    }
+    myOrder()*/
+
+
+
+
+
+
+let weather = new Promise ((resolve, reject) => {
+    setTimeout(() => 
+  //  resolve(), 3000)
+      resolve(), 3000)
+})
+
+    
+  weather
+    .then(() => {
+        console.log('its a beautiful day')
+})
+    .catch(() => {
+        console.log('its a gloomy day')
+
+})
+
+console.log('whats the weather today?')
+
+
+/* 
+const promise = new Promise ((resolved, rejected) => {
+    setTimeout(() => 
+        resolved(), 2000)
+})
+
+  promise 
+    .then(() => {
+        console.log('i kept the promise')
+    })
+    .catch(() => {
+        console.log('I did not keep the promise')
+    
+})
+//this worked as intended 😆 
 */
 
-
+    
 
 
 
