56987c106c77e2775fdf3c3a04a3bcc1e793e92b
sly750 <calvarez1007@yahoo.com>
2024-07-17 09:12:51 -0400

    replaced normal functions for arrows in appJs

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 let count = 0;
 
-document.querySelector("#btn1").onclick = function(){
-    count ++;
-    document.querySelector("#myLabel").innerHTML = count;
+document.getElementById('btn1').onclick = () => {
+    count++;
+    document.getElementById('myLabel').innerHTML = count;
 }
-document.querySelector("#btn2").onclick = function(){
+document.getElementById('btn2').onclick = () => {
     count = 0;
-    document.querySelector("#myLabel").innerHTML = count;
+    document.getElementById('myLabel').innerHTML = count;
 }
-document.querySelector("#btn3").onclick = function(){
-    count --;
-    document.querySelector("#myLabel").innerHTML = count;
+document.getElementById('btn3').onclick = () => {
+    count--;
+    document.getElementById('myLabel').innerHTML = count;
 }
